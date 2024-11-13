e611031b2a642dbac85e9678ef8b65430373620f
Mizkus <kirill131c@gmail.com>
2024-11-13 17:22:59 +0700

    upload labs

---
+++ dec_timer.py
@@ -0,0 +1,11 @@
+import time
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Elapsed time: {end_time - start_time}")
+        return result
+    return wrapper
+
